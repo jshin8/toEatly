3b710a8983feca5e6d49a95c8519b62b834a5814
jshin8 <joshshin8@gmail.com>
2015-10-14 21:45:31 -0700

    delete functionality added

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,57 @@
 // CLIENT-SIDE JAVASCRIPT
+// toEatly/public/js/app.js
+// served with express.static in server.js
 // On page load
 $(document).ready(function(){
-  
+	console.log("I'm super sure this is connected to index");
+	$("#new-food-form").on('submit', function(e){
+		// prevent default submit behavior
+		e.preventDefault();
+		// var newFoodData = {};
+		// newFoodData.name = $("#name").val();
+		// newFoodData.yumminess = $("#yum").val();
+		// make an ajax post request to /api/foods with the new food data
+		$.ajax({
+			type: 'POST',
+			url: '/api/foods',
+			data: $('#new-food-form').serialize(),
+			// data: newFoodData,
+			success: function(response){
+				console.log("post response: ",response);
+				$("#new-food-form")[0].reset();
+				// actually add new food to the page
+				addFoodToPage(response);
+			}
+		});
+	});
+	$(document).on('click', 'button.close', function(e){
+		deleteFood(this);
+		// make an ajax DELETE request to our new route
+		// use the food's id in the url
+		// when successful, remove it from the page
+	});
 });
+function addFoodToPage(response){
+	// create a new li with the response food data
+	var htmlString = "<li class='list-group-item'>\n" + 
+          response.name + 
+          "<span class='label label-default'>" +
+          response.yumminess + "</span>" +
+        "</li>";
+	// add the li to the page with append
+	$("#food-ul").append(htmlString);
+}
+
+function deleteFood(context){
+	console.log(context);
+	var foodId = $(context).data().id;
+	$.ajax({
+	    url: '/api/foods/' + foodId,
+	    type: 'DELETE',
+	    success: function(response) {
+	      // once successful, remove food from the DOM
+	      $(context).closest('li').remove();
+    }
+  });
+
+}
--- server.js
+++ server.js
@@ -6,11 +6,16 @@ var express = require("express"),
     path = require("path"),
     bodyParser = require("body-parser");
 
+//require the where function
+var where = require("./utils/where");
+
 // CONFIG //
 // set ejs as view engine
 app.set('view engine', 'ejs');
 // serve js & css files
 app.use("/static", express.static("public"));
+
+  // from index.ejs: <script src="/static/js/app.js"></script>
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,54 +28,66 @@ var foods =[
   {id: 4, name: "Kale", yumminess: "meh"}
 ];
 
-app.get('/', function(req,res){
+// ROUTES //
+app.get("/", function (req, res){
+  // render index.html and send with foods data filled in
   res.render('index', {foods: foods});
 });
 
-// app.post('/api/foods', function(req,res){
-//   var newFood = {};
-//    //we need the newFood object to have id attribute(unique), name attribute, yumminess attribute
-//   for(var j=0;j<foods.length;j++){
-//   newFood.id=foods.length;
-//   }
- 
+// api route to get all foods
+app.get("/api/foods", function (req, res){
+  // send foods data as JSON
+  res.json(foods);
+});
 
-//   newFood.name=req.body.name;
-//   newFood.yumminess=req.body.yumminess;
-
-
-//   foods.push(newFood); //add to foods array
-//   res.json(newFood);
-//    //res.render('index', {foods: foods});
-// });
-
-app.post('/api/foods', function(req,res){
+// create a new food object
+app.post('/api/foods', function(req, res){
   console.log(req.body);
+  // create a new food object
   var newFood = {};
-   
+  // for (var j=0; j < foods.length; j++){
+  //  newFood.id = foods.length;
+  // }
+  // add an id
+  // loop through array, find max number, and add one
   if (foods.length > 0) {
     var maxId = foods[0].id;
     for (var j=0; j<foods.length; j++){
       if ( foods[j].id > maxId){
         maxId = foods[j].id;
       }
-     // maxId = Math.max(foods[j-1].id, foods[j].id); (works only if list is in order, smallest to greatest)
+      // maxId = Math.max(foods[j-1].id, foods[j].id); (works only if list is in order, smallest to greatest)
     }
-   // now maxId is highest id in array
-        newFood.id = maxId + 1;
-  }   
-  else { // foods array is empty
-        newFood.id = 0;
+    // now maxId is highest id in array
+    newFood.id = maxId + 1;
+  } else { // foods array is empty
+    newFood.id = 0;
   }
-
-
-  newFood.name=req.body.name;
-  newFood.yumminess=req.body.yumminess;
-
+  newFood.name = req.body.name;
+  newFood.yumminess = req.body.yumminess;
+  // add our new food object to the foods array
   foods.push(newFood);
   res.json(newFood);
- 
 });
+
+// delete a food item
+app.delete('/api/foods/:id', function(req, res){
+  var targetId = parseInt(req.params.id);
+  // find item in the array matching the id
+  var targetItem = where(foods, {id: targetId});
+  // get the index of the found item
+  var index = foods.indexOf(targetItem);
+  // remove the item at that index, only remove 1 item
+  foods.splice(index, 1);
+  // send back deleted object
+  res.json(targetItem);
+});
+
+
+
+
+
+
 
 app.listen(3000, function (){
   console.log("listening on port 3000");
